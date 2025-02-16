1f4487d4a5b0b875f250232c1209c14e5279ef9a
Vishwa <135529226+vishwaDamruth@users.noreply.github.com>
2025-02-17 00:21:25 +0530

    Logic for incrementing scores is implemented

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -18,3 +18,47 @@ let computerchoice = function(){
     }
 }
 
+
+
+let one = computerchoice();
+let two = humanchoice();
+
+let one_score =0;
+let two_score = 0;
+
+
+
+for(let i = 0; i < 5;i++){
+
+    one = computerchoice();
+    two = humanchoice();  
+    
+    one = one.toLocaleLowerCase();
+    two = two.toLocaleLowerCase();
+
+    if(one == "rock" && two == "paper"){
+        two_score++;
+    }
+    else if(one == "rock" && two == "scissors"){
+        one_score++;
+    }
+    else if(one == "paper" && two == "rock"){
+        one_score++;
+
+    }
+
+    else if(one == "paper" && two == "scissors"){
+        two_score++;
+    }
+
+    else if(one == "scissors" && two == "rock"){
+        two_score++;
+    }
+
+    else if(one == "scissors" && two == "paper"){
+        one_score++;
+    }
+
+}
+
+console.log(one_score + " " + two_score);
